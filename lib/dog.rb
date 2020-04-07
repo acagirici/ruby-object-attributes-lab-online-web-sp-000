8ed6df97988bd28357a5654dc80bd51324ffee98
Arif Cagirici <acagirici23@gmail.com>
2020-04-07 01:58:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,3 +16,20 @@
   #end
 #end
 
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
